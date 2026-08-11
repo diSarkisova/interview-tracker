1791b3017444af66d46c1681a0db33c4eed2c309
diSarkisova <diana.sarkisova1999@gmal.com>
2026-08-11 16:09:12 +0300

    IT-1: add application layouts

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,15 +1,31 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div class="default-layout">
-    <header></header>
+    <header>
+      <router-link to="/">Трекер собеседований</router-link>
+
+      <div>
+        <router-link to="/">Доска</router-link>
+        <router-link to="/interviews">Таблица</router-link>
+        <router-link to="/consultations">Консультации</router-link>
+
+        <div>
+          <button @click="onAddForm">Добавить +</button>
+        </div>
+      </div>
+    </header>
     <main>
       <RouterView/>
     </main>
     <footer></footer>
   </div>
 </template>
+
+<script setup lang="ts">
+function onAddForm() {
+  console.log('onAddForm')
+}
+</script>
+
 
 <style scoped>
 </style>
